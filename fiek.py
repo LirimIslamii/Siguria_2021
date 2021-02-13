3b5f1fbe66f3422d5da0b5aee4485cfe65d3d9a7
jonrexhbogaj <61279448+jonrexhbogaj@users.noreply.github.com>
2021-02-13 19:44:04 +0100

    Update fiek.py

--- fiek.py
+++ fiek.py
@@ -11,23 +11,14 @@ from tkinter import messagebox
 from tkinter.filedialog import askopenfile
 import time
 from tkinter import Entry
-from tkinter import font
 
-
-def hap():
-    try:
-        filename = askopenfile(mode='r',filetypes=[("Files","*.txt")])
-        content = filename.read()
-        messagebox.showinfo("Connected",content)
-    except:
-        messagebox.showerror("Error","Ju Lutem Specifikoni nje Program!")
 def ndihma():
     messagebox.showinfo("Kontribues","\n".join(["Lirim Islami©","Uran Lajqi©","Jon Rexhbogaj©"]))
 
 
 def resize(e):
     global image1, resizer,image2
-    image1 = Image.openi("C:\\Users\\Uran\\Desktop\\Python\\Siguria_2021-main\\j.jpg")
+    image1 = Image.open("C:\\Users\\DELL\\Desktop\\New folder\\j.jpg")
     resizer = image1.resize((e.width,e.height), Image.ANTIALIAS)
     image2 = ImageTk.PhotoImage(resizer)
     canvas.create_image(0,0,anchor=NW,image=image2)
@@ -108,14 +99,14 @@ def struktura():
 
     tv.heading('#0',text='Dir：'+directory,anchor='w')
     path=os.path.abspath(directory)
-    node=tv.insert('','end',text=path,openi=True)
+    node=tv.insert('','end',text=path,open=True)
 
     def traverse_dir(parent,path):
         if os.access(directory,os.X_OK):
             for d in os.listdir(path):
                 full_path=os.path.join(path,d)
                 isdir = os.path.isdir(full_path)
-                id=tv.insert(parent,'end',text=d,openi=False)
+                id=tv.insert(parent,'end',text=d,open=False)
                 try:
                     if isdir:
                         traverse_dir(id,full_path)
@@ -128,12 +119,22 @@ def struktura():
     ybar.pack(side=RIGHT,fill=Y)
     tv.pack()
 
-def openi():
+def help():
+    global help
+    help = Toplevel(window,bg='#ccffcc')
+    help.geometry("300x300")
+    help.title("Help Index")
+    help.resizable(0,0)
+    label = Label(help,text="Hello",bg="#ccffcc",font=("Perpetua",12))
+    label.pack()
+    help.mainloop()
+
+
+def jep():
     global inter
     inter = Toplevel(window)
     inter.title("Notepad")
     #save dhe save as
-    #----------------------------------------------------------------------------------------------------------------------------------------
     global open_status_name
     open_status_name = False
 
@@ -142,12 +143,12 @@ def openi():
 
     # Ruaje si fajll
     def save_as_file():
-        text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/gui/", title="Save File", 
+        text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/gui/", title="Save File",
                     filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
         if text_file:
             name = text_file
             name = name.replace("C:/gui/", "")
-            window.title(f'{name} - TextPad!')
+            window.title(f'{name} - Success!')
 
             # Ruan fajllin
             text_file = open(text_file, 'w')
@@ -181,41 +182,20 @@ def openi():
     my_menu.add_cascade(label="File", menu=file_menu)
     file_menu.add_command(label="Save", command=save_file)
     file_menu.add_command(label="Save As...", command=save_as_file)
-    #----------------------------------------------------------------------------------------------------------------------------------------
-    inter.mainloop()
 
-def hiqe():
-    try:
-        if f.winfo_exists():
-            f.destroy()
-        if inter.winfo_exists():
-            inter.destroy()
-    except:
-        messagebox.showerror("Error","Nuk eshte e hapur asnje dritare!")
-
-def help():
-    global help
-    help = Toplevel(window,bg='#ccffcc')
-    help.geometry("300x300")
-    help.title("Help Index")
-    help.resizable(0,0)
-    label = Label(help,text="Hello",bg="#ccffcc",font=("Perpetua",12))
-    label.pack()
-    help.mainloop()
 
 window = Tk()
 window.title("Siguri 2021©")
-icon = PhotoImage(file = "C:\\Users\\Uran\\Desktop\\Python\\Siguria_2021-main\\k.png")
+icon = PhotoImage(file = "C:\\Users\\DELL\\Desktop\\New folder\\k.png")
 window.iconphoto(False,icon)
 canvas = Canvas(window,width=500, height=300)
 canvas.pack(fill="both", expand=True)
-my_image = ImageTk.PhotoImage(file="C:\\Users\\Uran\\Desktop\\Python\\Siguria_2021-main\\j.jpg")
+my_image = ImageTk.PhotoImage(file="C:\\Users\\DELL\\Desktop\\New folder\\j.jpg")
 canvas.create_image(0,0,anchor=NW,image=my_image)
 menubar = Menu(window)
 filemenu = Menu(menubar,tearoff=0)
-filemenu.add_command(label="New", command=openi)
+filemenu.add_command(label="New", command=jep)
 filemenu.add_command(label="Open",command=struktura)
-filemenu.add_command(label="Close",command=hiqe)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
@@ -231,7 +211,6 @@ helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="Help Index", command=help)
 helpmenu.add_command(label="About...",command=ndihma)
 menubar.add_cascade(label="Help", menu=helpmenu)
-
 
 
 
